bd6e3f60d30b6a42bf2dea284c7b2ed4504a8d06
t-kusakabe <ttmac0704@gmail.com>
2020-10-19 23:02:57 +0900

    Create component

---
+++ components/count.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Button, Text } from 'react-native';
+
+const Count = (props: any) => {
+  return (
+    <View>
+      <Button title='+' onPress={ () => props.addCount(props.count.count) } />
+      <Text>Count : { props.count.count }</Text>
+    </View>
+  );
+};
+
+export default Count;
