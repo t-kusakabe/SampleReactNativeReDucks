344ea1262a1a3460713fc418178ddc4c6a557930
t-kusakabe <ttmac0704@gmail.com>
2020-10-19 23:52:59 +0900

    ReactNativeには不要

--- configureStore.ts
+++ configureStore.ts
@@ -2,12 +2,6 @@ import { combineReducers, createStore } from "redux";
 
 import { countReducer } from "./modules/count";
 
-declare global {
-  interface IWindow {
-    store: any;
-  }
-}
-
 const store = createStore(
   combineReducers({
     count: countReducer
